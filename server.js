5a6a8ebc57b280ddc0a0d0ee7f93045d745130f5
J.C. Novoa <jcnovoa@hotmail.com>
2018-12-31 17:25:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-const http = require('http')
-const port = 3000
-
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+var http = require('http');
+http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Hello World\n');
+}).listen(3000);
+console.log('Server ready');
