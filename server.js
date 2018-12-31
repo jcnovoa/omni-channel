1c0685e80040e3e595f2fe73fc56fef015b5c95e
J.C. Novoa <jcnovoa@hotmail.com>
2018-12-31 18:05:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,19 @@ var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 var fs = require('fs');
-
 http.createServer(function(req, res) {
   var pathName = url.parse(req.url).pathname;
-  
   fs.readFile(__dirname + pathName, function(err, data){
     if(err){
           res.writeHead(404, {
             'Content-Type': 'text/plain'
           });
-          res.write('Page Not Found');
-          res.end(); 
+          res.write('Page Not Found' + JSON.stringify(err));
+          res.end();
        }else{
-          res.writeHead(202);
+          res.writeHead(200);
           res.write(data);
-          res.end();  
+          res.end();
        }
   });
 }).listen(80);
